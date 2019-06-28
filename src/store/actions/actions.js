01dc62ef2674e803248ddc138100fe8c11d847f6
LironWeiner <lironweiner10@gmail.com>
2019-06-28 18:35:32 +0300

    Clean code in actions.js

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -3,12 +3,6 @@ import { instance, apiKey, pageSize } from '../../axios-news';
 import { getCalculatedFromDate } from '../utility';
 import { pathNames } from '../../pathList';
 
-// export const pageIncrement = () => {
-//     return {
-//         type: actionTypes.PAGE_INCREMENT
-//     };
-// };
-
 export const fetchNewsFailed = (error) => {
     return {
         type: actionTypes.FETCH_NEWS_FAILED,
@@ -35,78 +29,46 @@ export const resetState = () => {
     };
 };
 
+const fetchFromApi = (dispatch, apiFetchString) => {
+    instance.get(apiFetchString)
+        .then(response => {
+            dispatch(setNews(response.data.articles));
+        })
+        .catch(error => {
+            dispatch(fetchNewsFailed(error));
+        });
+};
+
 export const initNews = (match) => {
-    return (dispatch, getState) => {        
+    return (dispatch, getState) => {
         switch (match.path) {
             case pathNames.Home:
-                instance.get('/top-headlines?country=il&pageSize=' + pageSize + '&apiKey=' + apiKey)
-                    .then(response => {
-                        dispatch(setNews(response.data.articles));
-                    })
-                    .catch(error => {
-                        dispatch(fetchNewsFailed(error));
-                    });
+                fetchFromApi(dispatch, '/top-headlines?country=il&pageSize=' + pageSize + '&apiKey=' + apiKey);
                 break;
             case pathNames.Us:
-                instance.get('/everything?q=politics+united states&from=' + getCalculatedFromDate(3) + '&language=en&sortBy=publishedAt&pageSize='
-                    + pageSize + '&page=' + getState().pageNumber + '&apiKey=' + apiKey)
-                    .then(response => {                        
-                        dispatch(setNews(response.data.articles));
-                    })
-                    .catch(error => {
-                        dispatch(fetchNewsFailed(error));
-                    });
+                fetchFromApi(dispatch, '/everything?q=politics+united states&from=' + getCalculatedFromDate(3) + '&language=en&sortBy=publishedAt&pageSize='
+                    + pageSize + '&page=' + getState().pageNumber + '&apiKey=' + apiKey);
                 break;
             case pathNames.World:
-                instance.get('/everything?q=world&from=' + getCalculatedFromDate(3) + '&sortBy=publishedAt&language=en&pageSize='
-                    + pageSize + '&page=' + getState().pageNumber + '&apiKey=' + apiKey)
-                    .then(response => {                        
-                        dispatch(setNews(response.data.articles));
-                    })
-                    .catch(error => {
-                        dispatch(fetchNewsFailed(error));
-                    });
+                fetchFromApi(dispatch, '/everything?q=world&from=' + getCalculatedFromDate(3) + '&sortBy=publishedAt&language=en&pageSize='
+                    + pageSize + '&page=' + getState().pageNumber + '&apiKey=' + apiKey);
                 break;
             case pathNames.Sports:
-                instance.get('/top-headlines?country=il&category=sports&pageSize=' + pageSize + '&apiKey=' + apiKey)
-                    .then(response => {
-                        dispatch(setNews(response.data.articles));
-                    })
-                    .catch(error => {
-                        dispatch(fetchNewsFailed(error));
-                    });
+                fetchFromApi(dispatch, '/top-headlines?country=il&category=sports&pageSize=' + pageSize + '&apiKey=' + apiKey);
                 break;
             case pathNames.Tech:
-                instance.get('/everything?q=technology+israel&sortBy=publishedAt&from=' + getCalculatedFromDate(3) + '&pageSize=' + pageSize + '&page=' + getState().pageNumber + '&apiKey=' + apiKey)
-                    .then(response => {
-                        dispatch(setNews(response.data.articles));
-                    })
-                    .catch(error => {
-                        dispatch(fetchNewsFailed(error));
-                    });
+                fetchFromApi(dispatch, '/everything?q=technology+israel&sortBy=publishedAt&from='
+                    + getCalculatedFromDate(3)
+                    + '&pageSize=' + pageSize + '&page=' + getState().pageNumber + '&apiKey=' + apiKey);
                 break;
             case pathNames.Business:
-                instance.get('/top-headlines?country=il&category=business&pageSize=' + pageSize + '&apiKey=' + apiKey)
-                    .then(response => {
-                        dispatch(setNews(response.data.articles));
-                    })
-                    .catch(error => {
-                        dispatch(fetchNewsFailed(error));
-                    });
+                fetchFromApi(dispatch, '/top-headlines?country=il&category=business&pageSize=' + pageSize + '&apiKey=' + apiKey);
                 break;
             case pathNames.Bitcoin:
-                {
-                    instance.get('/everything?q=bitcoin&from=' + getCalculatedFromDate(10)
-                        + '&sortBy=publishedAt&language=en&pageSize='
-                        + pageSize + '&page=' + getState().pageNumber + '&apiKey=' + apiKey)
-                        .then(response => {
-                            dispatch(setNews(response.data.articles));
-                        })
-                        .catch(error => {
-                            dispatch(fetchNewsFailed(error));
-                        });
-                    break;
-                }
+                fetchFromApi(dispatch, '/everything?q=bitcoin&from=' + getCalculatedFromDate(10)
+                    + '&sortBy=publishedAt&language=en&pageSize='
+                    + pageSize + '&page=' + getState().pageNumber + '&apiKey=' + apiKey);
+                break;
             default:
                 break;
         }
